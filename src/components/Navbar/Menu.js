68e5eab3cd7a2847dc1ff23cb715cacea3bc7cc9
maitzeth <andre801mz@gmail.com>
2018-10-02 21:57:40 -0300

    adding color to navbar

--- src/components/Navbar/Menu.js
+++ src/components/Navbar/Menu.js
@@ -20,9 +20,9 @@ class Menu extends Component {
     const { items } = this.props;
 
     return (
-      <Navbar className="fixed-top">
+      <Navbar className="fixed-top bg-white p-0 px-3">
         <NavbarBrand href="/">
-          <img src={myLogo} alt="Logo Navbar" style={{ height: 'auto', width: '80px', display: 'block' }} />
+          <img src={myLogo} alt="Logo Navbar" style={{ height: 'auto', width: '60px', display: 'block' }} />
         </NavbarBrand>
         <Nav className="ml-auto" navbar>
           <NavbarToggler handleToggler={this.handleToggler} isOpen={isOpen} />
