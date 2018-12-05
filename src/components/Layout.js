5cfad80ce150eb8398aaaa6d1db9b49082fdb3f4
maitzeth <andre801mz@gmail.com>
2018-12-05 15:04:10 -0300

    Adding bootstrap

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,4 @@
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { graphql, StaticQuery } from 'gatsby';
 import React, { Fragment } from 'react';
 import Foot from './Foot';
