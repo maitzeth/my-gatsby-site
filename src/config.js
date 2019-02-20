ce0f8bb9ad4bb0a45299b2b6e50ec95652ef0048
maitzeth <andre801mz@gmail.com>
2019-02-20 20:16:35 -0300

    Updating config for SEO

--- src/config.js
+++ src/config.js
@@ -2,12 +2,13 @@ module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
   siteTitle: 'André Iván', // Navigation and Site Title
   siteTitleAlt: 'André Iván - Web Developer Portfolio', // Alternative Site title for SEO
-  siteHeadline: 'Creating marvelous art & blazginly fast websites', // Headline for schema.org JSONLD
+  siteHeadline: 'Creating marvelous & blazing fast websites', // Headline for schema.org JSONLD
   siteTitleShort: 'André', // short_name for manifest
   siteUrl: 'https://andremz.me', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logo.png', // Used for SEO and manifest
-  siteDescription: 'Minimalistic bright portfolio with full-width grid and large images',
+  siteDescription:
+    'Minimalistic portfolio for a web developer, using GatsbyJS.',
   author: 'Maitzeth', // Author for schema.org JSONLD
 
   userTwitter: '@maitzeth', // Twitter Username
@@ -25,12 +26,22 @@ module.exports = {
       s: '768px',
       m: '992px',
       l: '1200px',
-    }
+    },
   },
   social: [
-    { id: 1, icon: 'FaFacebookF', title: 'Facebook', url: 'http://facebook.co' },
+    {
+      id: 1,
+      icon: 'FaFacebookF',
+      title: 'Facebook',
+      url: 'http://facebook.co',
+    },
     { id: 2, icon: 'FaTwitter', title: 'Twitter', url: 'http://twitter.com' },
-    { id: 3, icon: 'FaInstagram', title: 'Instagram', url: 'http://instagram.com' },
-    { id: 4, icon: 'FaGithub', title: 'Github', url: 'http://github.com' }
+    {
+      id: 3,
+      icon: 'FaInstagram',
+      title: 'Instagram',
+      url: 'http://instagram.com',
+    },
+    { id: 4, icon: 'FaGithub', title: 'Github', url: 'http://github.com' },
   ],
 }
